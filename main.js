ada09a6028c0b698150d1fd8502921d4f6f40788
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2023-06-09 05:24:18 +0200

    animacoes e transicoes

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ function createGame(player1, hour, player2) {
 `;
 }
 
+let delay = -0.3;
 function createCard(day, date, games) {
+  delay += 0.3;
   return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
           <h2>${day} <span>${date}</span></h2>
           <ul>
             ${games}
@@ -20,13 +22,22 @@ function createCard(day, date, games) {
 }
 
 document.querySelector("#cards").innerHTML += `
-        ${createCard("24/11", "quinta",
-        createGame('brazil', '16:00', 'serbia') + 
-        createGame('portugal', '16:00', 'uruguai'))}
+        ${createCard(
+          "24/11",
+          "quinta",
+          createGame("brazil", "16:00", "serbia") +
+            createGame("portugal", "16:00", "uruguai")
+        )}
 
-        ${createCard("28/11", "segunda", 
-        createGame('switzerland', '13:00', 'brazil'))}
+        ${createCard(
+          "28/11",
+          "segunda",
+          createGame("switzerland", "13:00", "brazil")
+        )}
 
-        ${createCard("02/12", "sexta",
-        createGame('brazil', '16:00', 'cameroon'))}
+        ${createCard(
+          "02/12",
+          "sexta",
+          createGame("brazil", "16:00", "cameroon")
+        )}
 `;
